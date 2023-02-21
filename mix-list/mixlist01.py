37630549bb17a5b7a1a3d58ecfc511a27ab35b69
brandi-jeff <brandi.jeff2@gmail.com>
2023-02-21 20:12:36 +0000

    mixed lists

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,14 @@
+#!user/bin/env python3
+
+# create the list
+my_list = ["192.168.0.5", 5060, "UP"]
+
+# print out each item in the list using concatenation
+#display first item
+print("The first item in the list (IP): " + my_list[0])
+
+#display second item
+print("The second item in the list (port): " + str(my_list[1]))
+
+#display last item
+print("The last item in the list (state): " + my_list[2])
